d4261e1fbc0c3544afc8ad03cefcbd5ec99e57ca
lucaskahl <lucaschangkahl@gmail.com>
2018-09-10 21:45:23 -0300

    Adicionando campo da aula

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -16,7 +16,56 @@ class UserController {
         this.formEl.addEventListener('submit', event => {
 
             event.preventDefault();
-            this.addValues(this.getValues());
+
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
+            let values = this.getValues();
+
+            this.getPhoto().then(
+                (content) => {
+                    values.photo = content;
+                    this.addLine(values);
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
+                },
+                (e) => {
+                    console.error(e);
+                }
+            );
+        });
+    }
+
+    getPhoto() {
+
+        return new Promise((resolve, reject) => {
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+                if (item.name === 'photo') return item;
+            });
+    
+            let file = elements[0].files[0];
+    
+            fileReader.onload = () => {
+    
+                resolve(fileReader.result);
+    
+            };
+
+            fileReader.onerror = (e) => {
+                reject(e);
+            };
+    
+            if (file) {
+                fileReader.readAsDataURL(file);
+            }else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
         });
     }
 
@@ -31,6 +80,10 @@ class UserController {
                 if( field.checked == true){
                     user[field.name] = field.value;
                 }
+            }else if(field.name == "admin"){ 
+                
+                user[field.name] = field.checked;
+
             }else{
                 user[field.name] = field.value;
             }
@@ -49,20 +102,23 @@ class UserController {
         );
     }
 
-    addValues(dataUser){
+    addLine(dataUser){
        
-        this.tableEl.innerHTML = `
-                        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-                        <td>${dataUser.name}</td>
-                        <td>${dataUser.email}</td>
-                        <td>${dataUser.admin}</td>
-                        <td>${dataUser.data}</td>
-                        <td>
-                            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                        </td>
-    
-        `;  
-    }
+        let tr = document.createElement('tr');
 
+        tr.innerHTML = `
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${(Utils.dateFormat(dataUser.register))}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+
+    `;  
+
+        this.tableEl.appendChild(tr);
+    }
 }
